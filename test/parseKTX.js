ba09c173345ce93450228ffe6d2ccdcd14cc58d7
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-03-29 21:05:54 +0200

    fix linting issue

--- test/parseKTX.js
+++ test/parseKTX.js
@@ -2,18 +2,18 @@ function parseKTX(buffer) {
 	const identifier = new Uint8Array(buffer, 0, 12);
 	console.log(identifier);
 
-	if (identifier[ 0 ] !== 0xAB ||
-		identifier[ 1 ] !== 0x4B ||
-		identifier[ 2 ] !== 0x54 ||
-		identifier[ 3 ] !== 0x58 ||
-		identifier[ 4 ] !== 0x20 ||
-		identifier[ 5 ] !== 0x31 ||
-		identifier[ 6 ] !== 0x31 ||
-		identifier[ 7 ] !== 0xBB ||
-		identifier[ 8 ] !== 0x0D ||
-		identifier[ 9 ] !== 0x0A ||
-		identifier[ 10 ] !== 0x1A ||
-		identifier[ 11 ] !== 0x0A
+	if (identifier[0] !== 0xAB ||
+		identifier[1] !== 0x4B ||
+		identifier[2] !== 0x54 ||
+		identifier[3] !== 0x58 ||
+		identifier[4] !== 0x20 ||
+		identifier[5] !== 0x31 ||
+		identifier[6] !== 0x31 ||
+		identifier[7] !== 0xBB ||
+		identifier[8] !== 0x0D ||
+		identifier[9] !== 0x0A ||
+		identifier[10] !== 0x1A ||
+		identifier[11] !== 0x0A
 	) {
 		console.error('Texture missing KTX identifier');
 	}
