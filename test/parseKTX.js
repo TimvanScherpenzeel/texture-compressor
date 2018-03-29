4538d1b3b4c6a08d44ba1e4c9ee84316ed989d75
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-03-29 21:20:07 +0200

    its reading the correct extension types

--- test/parseKTX.js
+++ test/parseKTX.js
@@ -1,6 +1,5 @@
-function parseKTX(buffer) {
+function parseKTX(buffer, facesExpected = 1) {
 	const identifier = new Uint8Array(buffer, 0, 12);
-	console.log(identifier);
 
 	if (identifier[0] !== 0xAB ||
 		identifier[1] !== 0x4B ||
@@ -18,16 +17,43 @@ function parseKTX(buffer) {
 		console.error('Texture missing KTX identifier');
 	}
 
+	// Load the rest of the header in 32 bit int
+	const header = new Int32Array(buffer, 12, 13);
+	const glType = header[1];
+	const glTypeSize = header[2];
+	const glFormat = header[3];
+	const glInternalFormat = header[4];
+	const glBaseInternalFormat = header[5];
+	const pixelWidth = header[6];
+	const pixelHeight = header[7];
+	const pixelDepth = header[8];
+	const numberOfArrayElements = header[9];
+	const numberOfFaces = header[10];
+	let numberOfMipmapLevels = header[11];
+	const bytesOfKeyValueData = header[12];
+
+	console.log(glInternalFormat);
+
+	if (glType !== 0) {
+		console.error('Only compressed formats are supported');
+	} else {
+		numberOfMipmapLevels = Math.max(1, numberOfMipmapLevels);
+	}
+
+	if (pixelHeight === 0 || pixelDepth !== 0) {
+		console.error('Only 2D textures are supported');
+	}
+
+	if (numberOfArrayElements !== 0) {
+		console.error('Texture arrays are not supported');
+	}
+
+	if (numberOfFaces !== facesExpected) {
+		console.error(`Number of faces expected ${facesExpected} but found ${numberOfFaces}`);
+	}
+
 	return 'ktx';
 
-	// const compression = 's3tc';
-	// const mipMapLevels = (header[DDS_HEADER_FLAGS] & DDSD_MIPMAPCOUNT) ? Math.max(1, header[DDS_HEADER_MIPMAPCOUNT]) : 1;
-	// const width = header[DDS_HEADER_WIDTH];
-	// const height = header[DDS_HEADER_HEIGHT];
-	// const dataOffset = header[DDS_HEADER_SIZE] + 4;
-	// const data = new Uint8Array(buffer, dataOffset);
-
-	// return {
 	// 	compression,
 	// 	mipMapLevels,
 	// 	width,
