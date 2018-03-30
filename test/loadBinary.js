21c7041057477484853ecd147d367058d8594b37
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-03-30 09:21:48 +0200

    remove file logging

--- test/loadBinary.js
+++ test/loadBinary.js
@@ -2,7 +2,6 @@ function loadBinary(filename) {
 	return new Promise((resolve, reject) => {
 		fileLoader(filename, 'arraybuffer')
 			.then((bin) => {
-				console.log(filename);
 				resolve(parseBinary(filename, bin));
 			})
 			.catch((error) => {
