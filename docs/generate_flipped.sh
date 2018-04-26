c4e326a7d6b6069d9ac55fc731f5804ddd3ad062
Tim van Scherpenzeel <timvanscherpenzeel@Tims-MacBook-Pro.local>
2018-04-26 09:58:58 +0200

    changed output directory from example to data to be in line with other projects

--- docs/generate_flipped.sh
+++ docs/generate_flipped.sh
@@ -5,10 +5,10 @@
 # Extension - https://www.khronos.org/registry/webgl/extensions/WEBGL_compressed_texture_astc/
 
 # example-astc-4x4
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-astc-4x4.ktx -m astc -c astc -b 4x4 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-astc-4x4.ktx -m astc -c astc -b 4x4 -y
 
 # example-astc-8x8
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-astc-8x8.ktx -m astc -c astc -b 8x8 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-astc-8x8.ktx -m astc -c astc -b 8x8 -y
 
 ### ETC
 
@@ -16,13 +16,13 @@ node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/fl
 # Extension - https://www.khronos.org/registry/webgl/extensions/WEBGL_compressed_texture_etc/
 
 # example-etc1
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-etc1.ktx -m etc -c etc1 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-etc1.ktx -m etc -c etc1 -y
 
 # example-etc2
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-etc2.ktx -m etc -c etc2 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-etc2.ktx -m etc -c etc2 -y
 
 # example-etc2A
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-etc2A.ktx -m etc -c etc2 -t -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-etc2A.ktx -m etc -c etc2 -t -y
 
 
 ### PVR
@@ -31,16 +31,16 @@ node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/fl
 # Extension - http://www.khronos.org/registry/webgl/extensions/WEBGL_compressed_texture_pvrtc/
 
 # example-pvrtc2BPP
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-pvrtc2BPP.ktx -m pvr -c pvrtc1 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-pvrtc2BPP.ktx -m pvr -c pvrtc1 -y
 
 # example-pvrtc2BPPA
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-pvrtc2BPPA.ktx -m pvr -c pvrtc1 -t -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-pvrtc2BPPA.ktx -m pvr -c pvrtc1 -t -y
 
 # example-pvrtc4BPP
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-pvrtc4BPP.ktx -m pvr -c pvrtc1 -b 4 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-pvrtc4BPP.ktx -m pvr -c pvrtc1 -b 4 -y
 
 # example-pvrtc4BPPA
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-pvrtc4BPPA.ktx -m pvr -c pvrtc1 -b 4 -t -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-pvrtc4BPPA.ktx -m pvr -c pvrtc1 -b 4 -t -y
 
 
 ### S3TC
@@ -49,13 +49,13 @@ node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/fl
 # Extension - http://www.khronos.org/registry/webgl/extensions/WEBGL_compressed_texture_s3tc/
 
 # example-dxt1
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-dxt1.ktx -m s3tc -c dxt1 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-dxt1.ktx -m s3tc -c dxt1 -y
 
 # example-dxt1A
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-dxt1A.ktx -m s3tc -c dxt1 -t -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-dxt1A.ktx -m s3tc -c dxt1 -t -y
 
 # example-dxt3
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-dxt3.ktx -m s3tc -c dxt3 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-dxt3.ktx -m s3tc -c dxt3 -y
 
 # example-dxt5
-node ./bin/texture-compressor -i ./docs/example/example.png -o ./docs/example/flippedY/example-dxt5.ktx -m s3tc -c dxt5 -y
+node ./bin/texture-compressor -i ./docs/data/example.png -o ./docs/data/flippedY/example-dxt5.ktx -m s3tc -c dxt5 -y
